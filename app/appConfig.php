5fd8cbcb91e01fa1c85bc62fa29b4bc2bdb55d9f
Santiago Chávez <sanxofon@users.noreply.github.com>
2020-12-22 15:29:56 -0600

    Repository created
    Files uploaded (Initial commit)

---
+++ app/appConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$appname = "Progressive App Template";
+$shortname = "app";
+$version = "v2.0";
+if(@$_GET['launcher']) $version = $version."b";
+$description = "Máxima compatibilidad con los nuevos dispositivos inteligentes Android";
+$servidor = "//localhost";
+$ruta = "/app/";
